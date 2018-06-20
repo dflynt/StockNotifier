2fe8d1ae9e4f4b8702f02f10ebdf741ac3e58cda
Daniel Flynt <danielflynt1@gmail.com>
2018-06-20 15:35:32 -0400

    login and user registration

--- StockNotifier/__init__.py
+++ StockNotifier/__init__.py
@@ -6,10 +6,10 @@ import sqlite3
 app = Flask(__name__)
 app.config.update(dict(SECRET_KEY="powerful secretkey", WTF_CSRF_SECRET_KEY="a secret key"))
 Bootstrap(app)
-conn = sqlite3.connect("stocknotifier.db")
+conn = sqlite3.connect("stocknotifier.db", check_same_thread=False)
 c = conn.cursor()
 c.execute('''CREATE TABLE IF NOT EXISTS Users (
-            ID VARCHAR(30), 
+            ID VARCHAR(35), 
             FirstName TEXT, 
             LastName TEXT, 
             Email TEXT, 
@@ -23,5 +23,34 @@ c.execute('''CREATE TABLE IF NOT EXISTS UserSettings (
             FOREIGN KEY (ID) REFERENCES Users(ID))''')
 conn.commit()
 
+def insertNewUser(firstName, lastName, email, password, phone, AccConfirmed):
+    token = uuid.uuid5(uuid.NAMESPACE_URL, "danielflynt1@gmail.com")
+    strToken = email
+    c.execute("SELECT * FROM Users WHERE ID =?", (str(token),))
+    ##
+    alreadyRegistered = c.fetchone()[0]
+    if not alreadyRegistered:
+        c.execute("INSERT INTO Users (ID, FirstName, LastName, Email,"
+                "Passw, Phone, AccConfirmed) VALUES (?,?,?,?,?,?,?)", 
+                (str(token), firstName, lastName, email, password, str(phone), AccConfirmed))
+        conn.commit()
+        return True
+    else:
+        return False
+def showUsers():
+    c.execute("SELECT * FROM Users")
+    row = c.fetchone()
+    return row[0] + " " + row[1] + " " + row[2]
+    conn.commit()
 
+def loginUser(email, password):
+    c.execute("SELECT * FROM Users WHERE email = ?", (email,))
+    row = c.fetchone()
+    if row:
+        if row[4] == password:
+            return True
+        else:
+            return False
+    else:
+        return False
 from . import views
--- StockNotifier/views.py
+++ StockNotifier/views.py
@@ -1,35 +1,39 @@
-from . import app
-from . import c, conn
-from flask import render_template, request, url_for
+from . import app, c, conn
+from . import insertNewUser, showUsers, loginUser
+from flask import render_template, request, url_for, render_template_string
 from .forms import RegisterForm, LoginForm, AddStockForm
 #views = Blueprint("views", __name__, template_folder="templates")
 
-@app.route('/home')
+@app.route('/')
 def index():
-    
     return render_template("home.html", msg="New Message")
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
-    if request.method == "POST" and form.validate():
+    if request.method == "POST":
         firstName = request.form['firstName']
         lastName = request.form['lastName']
         email = request.form['email']
-        username = request.form['username']
-        password = request.form['username']
+        password = request.form['password']
         phone = request.form['phone']
-        print(firstName + " " + lastName)
+        if insertNewUser(str(firstName), str(lastName), str(email), str(password), str(phone), "0"):
+            return render_template_string("<h5>Registered</h5>")
+        else:
+            return render_template_string("<h5>Already registered</h5>")
     else:
         return render_template("register.html")
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def signin():
+    if request.method == "POST":
+        email = request.form['email']
+        password = request.form['password']
+        if loginUser(email, password):
+            return render_template("home.html", msg="Hello world")
+        else:
+            return render_template_string("<h5>Invalid login</h5>")
     return render_template("login.html")
 
 @app.route("/dashboard")
 def dashboard():
     return "Dashboard"
-
-@app.route("/")
-def home():
-    return "change"
