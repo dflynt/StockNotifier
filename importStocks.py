1245fbb1f270037fb5ac6c9f93794c8a8bbb6599
Daniel Flynt <danielflynt1@gmail.com>
2018-06-21 17:01:20 -0400

    removed importStocks2.py

--- importStocks.py
+++ importStocks.py
@@ -37,8 +37,49 @@ with open ('NASDAQ.csv', newline='') as csvfile:
             c.execute("INSERT INTO Stocks (Symbol, Security, Sector)"
                     "VALUES (?,?,?)", 
                     (str(symbol), str(security), str(sector)))
+with open ('NYSE.csv', newline='') as csvfile:
+    stockreader = csv.reader(csvfile)
+    firstLine = True
+    for row in stockreader:
+        if firstLine: #skip header line
+            firstLine = False
+            continue
+        else:
+            symbol = row[0]
+            security = row[1]
+            sector = row[5]
+            c.execute("INSERT INTO Stocks (Symbol, Security, Sector)"
+                    "VALUES (?,?,?)", 
+                    (str(symbol), str(security), str(sector)))
 
+with open ('DOWJONES.csv', newline='') as csvfile:
+    stockreader = csv.reader(csvfile)
+    firstLine = True
+    for row in stockreader:
+        if firstLine: #skip header line
+            firstLine = False
+            continue
+        else:
+            symbol = row[2]
+            security = row[0]
+            sector = row[4]
+            c.execute("INSERT INTO Stocks (Symbol, Security, Sector)"
+                    "VALUES (?,?,?)", 
+                    (str(symbol), str(security), str(sector)))
+
+with open ('AMEX.csv', newline='') as csvfile:
+    stockreader = csv.reader(csvfile)
+    firstLine = True
+    for row in stockreader:
+        if firstLine: #skip header line
+            firstLine = False
+            continue
+        else:
+            symbol = row[0]
+            security = row[1]
+            sector = row[5]
+            c.execute("INSERT INTO Stocks (Symbol, Security, Sector)"
+                    "VALUES (?,?,?)", 
+                    (str(symbol), str(security), str(sector)))
 
 conn.commit()
-for row in c.execute('SELECT * FROM Stocks ORDER BY Symbol'):
-        print(row)
